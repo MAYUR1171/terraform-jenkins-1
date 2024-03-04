bb8e36b8f4ba6010b2903902465f61e682653da5
MAYUR1171 <157444461+MAYUR1171@users.noreply.github.com>
2024-03-04 10:21:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-22"
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-22-777"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "its-my-dynamodb-table"
   }
 }
